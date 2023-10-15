5aa1c94ddd5303a576ff07ed0e750ba69e9c4be1
Ritik Thakur <hritikthakur1947@gmail.com>
2023-10-16 00:35:49 +0530

    schema

--- build/index.js
+++ build/index.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const server_1 = require("@apollo/server");
 const express4_1 = require("@apollo/server/express4");
+const db_1 = require("./lib/db");
 function init() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = (0, express_1.default)();
@@ -27,12 +28,32 @@ function init() {
             hello : String,
             say(name:String):String,
         }
+        type Mutation {
+            createUser(firstName: String!,
+                 lastName: String! ,
+                 email:String!,
+                 password:String!): Boolean
+        }
     `,
             resolvers: {
                 Query: {
                     hello: () => `hey there  i am qraphql server `,
                     say: (_, { name }) => `HEY ${name} , how are you ?`
                 },
+                Mutation: {
+                    createUser: (_, { firstName, lastName, email, password }) => __awaiter(this, void 0, void 0, function* () {
+                        yield db_1.prismaClient.user.create({
+                            data: {
+                                email,
+                                firstName,
+                                lastName,
+                                password,
+                                salt: "random_value",
+                            }
+                        });
+                        return true;
+                    })
+                }
             },
         });
         app.get("/", (req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 import { json } from 'body-parser';
+import { prismaClient } from './lib/db';
 
 async function init(){
     const app = express()
@@ -16,12 +17,46 @@ const gqlServer =new ApolloServer({
             hello : String,
             say(name:String):String,
         }
+        type Mutation {
+            createUser(firstName: String!,
+                 lastName: String! ,
+                 email:String!,
+                 password:String!): Boolean
+        }
     `,
     resolvers :{
         Query :{
             hello:()=>`hey there  i am qraphql server `,
             say :(_,{name}:{name:string})=>`HEY ${name} , how are you ?`
         },
+        Mutation :{
+            createUser: async (_,{
+                firstName,
+                lastName ,
+                email,
+                password
+            }:{
+                 firstName:string;
+                lastName : string;
+                email : string ;
+                password :string;
+            })=>{
+                    await prismaClient.user.create ({   
+                        
+                        data:{
+                            email,
+                            firstName,
+                            lastName,
+                            password,
+                            salt:"random_value",
+                            
+                        }                
+                    
+                })
+                return true;
+                    
+                }     
+        }
     },
 }
 )
